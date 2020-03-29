746d8ca0569c5cbb4bb963f12b9f0f63b5316c5c
Guilherme Akira <guiakira.demenechmori@gmail.com>
2020-03-29 16:36:38 -0300

    26/03/27/03
    fração sobre fração

---
+++ mobile/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Routes from './src/routes'
+/*import { StyleSheet, Text, View } from 'react-native'
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+*/
+export default function App() {
+  return <Routes></Routes>
+}
---
+++ mobile/src/pages/Casos/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View} from 'react-native'
+import imgLogo from '../../assets/logo.png'
+import styles from './styles'
+
+export default function Casos () {
+    return (
+        <View>
+            
+        </View>
+    )
+}
